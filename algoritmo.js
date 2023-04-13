e0dce216f246b187eb90fb6645cb24fc13911b23
Troyano97 <ivanferreyradaniel@gmail.com>
2023-04-12 23:00:28 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,5 @@
+let nombreIngresado = prompt ("como te llamas?");
+
+document.querySelector("body"). innerHTML = "Me llamo? "+ nombreIngresado;
+
+document.write ("<h1> me llamo: " + nombreIngresado + "</h1>" );
